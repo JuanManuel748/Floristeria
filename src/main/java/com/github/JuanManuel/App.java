ecb9abca705bd5214b65255e9282ef9f12089f33
JuanMa748 <junperz163@gmail.com>
2024-12-02 21:04:04 +0100

    DAOcomentados

--- src/main/java/com/github/JuanManuel/App.java
+++ src/main/java/com/github/JuanManuel/App.java
@@ -12,12 +12,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-/**
- * JavaFX App
- */
 public class App extends Application {
-
-    // Atributos estáticos que permiten el acceso global a la escena, al escenario y al controlador actual.
     public static Scene scene;
     public static Stage stage;
     public static AppController currentController;
@@ -34,20 +29,16 @@ public class App extends Application {
     public void start(Stage primaryStage) throws Exception {
         stage = primaryStage;
 
-        // Cargar la vista inicial (definida en el archivo FXML correspondiente)
         View view = AppController.loadFXML(Scenes.ROOT);
 
-        // Crear una escena con la vista cargada, configurando las dimensiones (800x600)
         scene = new Scene(view.scene, 800, 600);
 
-        // Obtener el controlador asociado a la vista cargada y llamar al método onOpen()
         currentController = (AppController) view.controller;
         currentController.onOpen(null);
 
-        // Configurar el escenario (ventana de la aplicación)
         stage.setScene(scene);
-        stage.setTitle("Floristería Reyes");  // Título de la ventana
-        stage.getIcons().add(new Image(getClass().getResourceAsStream("/com/github/JuanManuel/view/images/logoBlanco.png")));  // Establecer icono para la ventana
-        stage.show();  // Mostrar la ventana
+        stage.setTitle("Floristería Reyes");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/com/github/JuanManuel/view/images/logoBlanco.png")));
+        stage.show();
     }
 }
